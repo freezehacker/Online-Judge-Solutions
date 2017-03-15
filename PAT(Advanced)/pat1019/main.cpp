e08d4dd13c11585bf0c24abaa88c0a5c46bbc91c
freezehacker <793245364@qq.com>
2017-03-15 11:04:18 +0800

    nothing

--- PAT(Advanced)/pat1019/main.cpp
+++ PAT(Advanced)/pat1019/main.cpp
@@ -1,9 +1,58 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 using namespace std;
 
-int main() {
-	ifstream cin("in");
+#define CASE_NUM 1
 
-	
+vector<int> reverseBigNumber;
+
+// Number in every digit is within an INT range.
+bool isPalindromic(int n, int base) {
+
+    do {
+        reverseBigNumber.push_back( n % base );
+        n /= base;
+    } while (n);
+
+    int length = reverseBigNumber.size();
+
+    for (int i = length / 2 - 1; i >= 0; --i) {
+        if (reverseBigNumber[i] != reverseBigNumber[length - 1 - i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+int main() {
+    //ifstream cin("in");
+
+    int caseNum = CASE_NUM;
+
+    while (caseNum--) {
+        reverseBigNumber.clear();
+
+        int n, base;
+        cin >> n >> base;
+
+        if ( isPalindromic(n, base) ) {
+            cout << "Yes\n";
+        } else {
+            cout << "No\n";
+        }
+
+        for (int i = reverseBigNumber.size() - 1; i >= 0; --i) {
+            if (i != reverseBigNumber.size() - 1) {
+                cout << " ";
+            }
+            cout << reverseBigNumber[i];
+        }
+
+        cout << endl;
+    }
+
+    return 0;
 }
